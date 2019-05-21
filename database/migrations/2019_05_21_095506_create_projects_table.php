0f42c4b1eedc30501627fed8826868d859570e40
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-21 15:53:48 +0100

    Update 2019_05_21_095506_create_projects_table.php

--- database/migrations/2019_05_21_095506_create_projects_table.php
+++ database/migrations/2019_05_21_095506_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer("client_id");
+            $table->integer("client_id")->unsigned();;
             $table->string("name");
             $table->string("details");
             $table->date("start_date");
