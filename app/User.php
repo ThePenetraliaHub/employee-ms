3e6f17d2abe8c78cceb4263829652d60d4298624
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:05:14 +0100

    Update User.php

--- app/User.php
+++ app/User.php
@@ -10,30 +10,42 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getUserAvatarAttribute()
+    {
+        return asset('storage/'.$this->avatar);
+    }
+    
+    public function typeable()
+    {
+        return $this->morphTo();
+    }
+
+    public function isActive()
+    {
+        return $this->is_active;
+    }
+
+    public function owner()
+    {
+        return $this->typeable();
+    }
+
+    public function user_type()
+    {
+        if ($this->typeable_type === "App\SuperAdmin") {
+            return "Super Administrator";
+        }
+    }
 }
