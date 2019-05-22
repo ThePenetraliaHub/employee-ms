a8d28440b992e09bfc7e68f9e71a071a3c0dbc0c
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:05:05 +0100

    Update Project.php

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function employees()
+    {
+        return $this->belongsToMany('App\Employee');
+    }
+
+    public function client()
+    {
+        return $this->belongsTo('App\Client');
+    }
+
+    public function supervisors()
+    {
+        return $this->belongsToMany('App\Employee', "employee_projects", "supervisor_id", 'project_id');
+    }
 }
