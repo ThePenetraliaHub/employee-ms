e76fc632500b61bc4c477c884573abf8fbe16213
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:05:09 +0100

    Update State.php

--- app/State.php
+++ app/State.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    public function employees()
+    {
+        return $this->hasMany('App\Employee');
+    }
+
+    public function cities()
+    {
+        return $this->hasMany('App\City');
+    }
+
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
 }
