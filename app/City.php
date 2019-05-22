e69c70ede731852e306a9f49d191f6530f1740a1
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:04:09 +0100

    Update City.php

--- app/City.php
+++ app/City.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    public function employees()
+    {
+        return $this->hasMany('App\Employee');
+    }
+
+    public function state()
+    {
+        return $this->belongsTo('App\State');
+    }
 }
