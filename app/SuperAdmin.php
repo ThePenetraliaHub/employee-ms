df3f335280090602c794871c88ddea42a27ab5c1
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:05:11 +0100

    Update SuperAdmin.php

--- app/SuperAdmin.php
+++ app/SuperAdmin.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SuperAdmin extends Model
 {
-    //
+    protected $guarded = [];
+
+	public function user_info()
+    {
+        return $this->morphOne('App\User', 'typeable');
+    }
 }
