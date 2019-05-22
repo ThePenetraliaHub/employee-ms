a65445d70e6bc8bf3cecf6e40a45ebc90ec2a66a
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:04:15 +0100

    Update Country.php

--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    public function employees()
+    {
+        return $this->hasMany('App\Employee');
+    }
+
+    public function states()
+    {
+        return $this->hasMany('App\State');
+    }
 }
