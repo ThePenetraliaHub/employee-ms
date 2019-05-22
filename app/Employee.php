8edab529bab932bee2d3dafb35aa4ccdd1ad0b14
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-22 04:05:01 +0100

    Update Employee.php

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,60 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $guarded = [];
+
+	public function user_info()
+    {
+        return $this->morphOne('App\User', 'typeable');
+    }
+
+    public function certifications()
+    {
+        return $this->hasMany('App\Certification');
+    }
+
+    public function educations()
+    {
+        return $this->hasMany('App\Educatution');
+    }
+
+    public function skills()
+    {
+        return $this->hasMany('App\Skill');
+    }
+
+    public function projects()
+    {
+        return $this->belongsToMany('App\Project');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo('App\Department');
+    }
+
+    public function country()
+    {
+        return $this->belongsTo('App\Department');
+    }
+
+    public function state()
+    {
+        return $this->belongsTo('App\Department');
+    }
+
+    public function city()
+    {
+        return $this->belongsTo('App\Department');
+    }
+
+    public function supervisor()
+    {
+        return $this->belongsTo('App\Employee', "supervisor_id");
+    }
+
+    public function supervise()
+    {
+        return $this->hasMany('App\Employee', "supervisor_id");
+    }
 }
