b53a35ea61093c8740ea20e6771caaf5fb4f60a2
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-23 14:20:57 +0100

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>PenTax</title>
+  <title>Penetralia Hub EMS</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
@@ -44,15 +44,106 @@
           <ul class="sidebar-menu" data-widget="tree">
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Tax Payer</span>
+                <i class="fa fa-dashboard"></i> <span>Admin</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="{{-- {{ route("user.individual.create") }} --}}"><i class="fa fa-circle-o"></i> Add Tax Payer</a></li>
-                <li><a href="{{-- {{ route("user.individual.search") }} --}}"><i class="fa fa-circle-o"></i> Search Tax Payer</a></li>
-                <li><a href="{{-- {{ route("user.individual") }} --}}"><i class="fa fa-circle-o"></i> View Tax Payers</a></li>
+                <li><a href="{{-- {{ route("user.individual.create") }} --}}"><i class="fa fa-circle-o"></i> Dashboard</a></li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-circle-o"></i> Administration
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i> Create Administrator</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i> View Administrators</a></li>
+                    </ul>
+                </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-circle-o"></i> Department
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i> Create Department</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i> View Departments</a></li>
+                    </ul>
+                </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-circle-o"></i> Client
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i> Create Client</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i> View Clients</a></li>
+                    </ul>
+                </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-circle-o"></i> Project
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="#"><i class="fa fa-circle-o"></i> Create Project</a></li>
+                        <li><a href="#"><i class="fa fa-circle-o"></i> View Projects</a></li>
+                    </ul>
+                </li>
+
+                <li class="treeview">
+                    <a href="#"><i class="fa fa-circle-o"></i> Job Setup
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="treeview">
+                            <a href="#"><i class="fa fa-circle-o"></i> Job Title
+                                <span class="pull-right-container">
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </span>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="#"><i class="fa fa-circle-o"></i> Add Job Title</a></li>
+                                <li><a href="#"><i class="fa fa-circle-o"></i> View Job Titles</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="treeview">
+                            <a href="#"><i class="fa fa-circle-o"></i> Pay Grade
+                                <span class="pull-right-container">
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </span>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="#"><i class="fa fa-circle-o"></i> Add Pay Grade</a></li>
+                                <li><a href="#"><i class="fa fa-circle-o"></i> View Pay Grades</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="treeview">
+                            <a href="#"><i class="fa fa-circle-o"></i> Employment Status
+                                <span class="pull-right-container">
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </span>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="#"><i class="fa fa-circle-o"></i> Add Emp. Status</a></li>
+                                <li><a href="#"><i class="fa fa-circle-o"></i> View Emp. Statuses</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
               </ul>
             </li>
 
