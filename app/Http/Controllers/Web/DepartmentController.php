582b3ab49db11c5ac01d65421bef398642da3c99
Lawal Ahmed Hamisu <=>
2019-05-23 10:55:53 +0100

    Department controller

--- app/Http/Controllers/Web/DepartmentController.php
+++ app/Http/Controllers/Web/DepartmentController.php
@@ -3,37 +3,79 @@
 namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
+use App\Department;
 
 class DepartmentController extends Controller
 {
     public function index()
     {
         //
+        $departments = Department::orderBy('id', 'desc')->paginate(10);
+
+        return view('pages.departments.list', ['departments' => $departments]);
     }
 
     public function create()
     {
         //
+        return view('pages.departments.create');
     }
 
     public function store(Request $request)
     {
         //
+        $rules = [
+            'department_name' => 'required|unique:departments,name'
+        ];
+
+        $customMessages = [
+            'department_name.required' => 'Please provide the department name.',
+        ];
+
+        $this->validate($request, $rules, $customMessages);
+
+        $department = Department::create([
+            'department_name' => $request->input('department_name'),
+        ]);
+
+        return redirect('department');
     }
 
     public function show($id)
     {
         //
+        $department = Department::findOrFail($id);
+
+        return view('pages.departments.show', ['department' => $department]);
     }
 
     public function edit($id)
     {
         //
+        $department = Department::findOrFail($id);
+
+        return view('pages.departments.edit', ['department' => $department]);
     }
 
     public function update(Request $request, $id)
     {
         //
+        $department = Department::findOrFail($id);
+
+        $rules = [
+            'department_name' => 'required|unique:departments,name,'.$id,
+        ];
+
+        $customMessages = [
+            'department_name.required' => 'Please provide the department name.',
+        ];
+
+        $this->validate($request, $rules, $customMessages);
+
+        $department->department_name = $request->input('department_name');
+        $department->save();
+
+        return redirect('departments/'.$id);
     }
 
     public function destroy($id)
