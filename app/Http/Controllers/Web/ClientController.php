5506891b85289000ab90c28c3c10c7250296fb30
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-23 10:31:28 +0100

    moved controllers to the web folder

---
+++ app/Http/Controllers/Web/ClientController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/Controllers/Web/EmployeeController.php
+++ app/Http/Controllers/Web/EmployeeController.php
@@ -8,11 +8,7 @@ use App\Http\Controllers\Controller;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         //
@@ -21,23 +17,12 @@ class EmployeeController extends Controller
         return view('pages.employees.list', ['employees' => $employees]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
         return view('pages.employees.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
@@ -49,12 +34,6 @@ class EmployeeController extends Controller
         return redirect('employees');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
@@ -63,12 +42,6 @@ class EmployeeController extends Controller
         return view('pages.employees.show', ['employee' => $employee]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
@@ -77,13 +50,6 @@ class EmployeeController extends Controller
         return view('pages.employees.edit', ['employee' => $employee]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
@@ -95,12 +61,6 @@ class EmployeeController extends Controller
         return redirect('employees/'.$id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
