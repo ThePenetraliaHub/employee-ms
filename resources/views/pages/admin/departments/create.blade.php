9d75ddffe5a805a7e4f77a10563179a46bd14de6
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-23 15:25:18 +0100

    Update create.blade.php

--- resources/views/pages/admin/departments/create.blade.php
+++ resources/views/pages/admin/departments/create.blade.php
@@ -15,23 +15,21 @@
                 <form {{-- autocomplete="off" --}} novalidate="novalidate" role="form" id="submit_form" class="form-horizontal" method="POST" action="{{ route('department.store') }}">
                   @csrf
                   <div class="box-body">
-                    <div class="box">
-                        <div class="box-body">
-                            <div class="form-row">
-                                <div class="form-group col-xs-6 ml-1{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    <label for="name">Name: </label>
-                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required placeholder="name">
-                                    
-                                    @if ($errors->has('name'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('name') }}</strong>
-                                        </span>
-                                    @endif
+                    {{-- <div class="box"> --}}
+                        <div class="form-row">
+                            <div class="form-group col-xs-6 ml-1{{ $errors->has('name') ? ' has-error' : '' }}">
+                                <label for="name">Name: </label>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required placeholder="name">
                                 
-                                </div>
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            
                             </div>
                         </div>
-                    </div>
+                    {{-- </div> --}}
                   </div>
                   <!-- /.box-body -->
                   <div class="text-center" id="form-errors"></div>
