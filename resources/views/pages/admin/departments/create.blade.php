b284afee5bf3aca4bd4006f84ce571581d188bb9
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-27 03:55:01 +0100

    update

--- resources/views/pages/admin/departments/create.blade.php
+++ resources/views/pages/admin/departments/create.blade.php
@@ -17,7 +17,7 @@
                         <div class="box-body">
                             <div class="form-row">
                                 <div class="form-group col-xs-11{{ $errors->has('name') ? ' has-error' : '' }} mb-0 mt-3">
-                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required placeholder="Department Name">
+                                    <input id="name" type="text" class="form-control " name="name" value="{{ old('name') }}" required placeholder="Department Name">
                                     
                                     @if ($errors->has('name'))
                                         <span class="help-block">
