08cddfe0107ee44986adf2fc51c94393bf85afb4
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-05-30 10:44:44 +0100

    update

--- resources/views/partials/_actionMessage.blade.php
+++ resources/views/partials/_actionMessage.blade.php
@@ -5,8 +5,8 @@
 @endif
 
 @if(Session::has('error'))
-	<div class="alert alert-success col-sm-6 col-sm-offset-6" role="alert">
-		{{Session::get('success')}}
+	<div class="alert alert-error col-sm-6 col-sm-offset-6" role="alert">
+		{{Session::get('error')}}
 	</div>
 @endif
 
--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -10,8 +10,8 @@
 
 <script type="text/javascript">
 	$(document).ready (function(){
-            $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-           		$("#success-alert").slideUp(500);
-            });   
+        $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+       		$("#success-alert").slideUp(500);
+        });   
  	});
 </script>
