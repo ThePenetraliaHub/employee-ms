74302f3609dc222f8d228031becc954f11e7d967
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-06-06 09:25:47 +0100

    Update create_employee_project.blade.php

--- resources/views/pages/admin/employee_project/forms/create_employee_project.blade.php
+++ resources/views/pages/admin/employee_project/forms/create_employee_project.blade.php
@@ -1,7 +1,7 @@
 <div class="form-row">
 
     <div class="form-group col-xs-11{{ $errors->has('client_id') ? ' has-error' : '' }} mb-0 mt-3">
-        <label for="client_id">Project Client</label>
+        <label for="client_id">Employee</label>
         <select class="form-control" id="client_id" name="client_id">
             <option value="">-- Select Client --</option>
             @foreach($clients as $client)
@@ -16,7 +16,7 @@
     </div>
 
     <div class="form-group col-xs-11{{ $errors->has('client_id') ? ' has-error' : '' }} mb-0 mt-3">
-        <label for="client_id">Project Client</label>
+        <label for="client_id">Project</label>
         <select class="form-control" id="client_id" name="client_id">
             <option value="">-- Select Client --</option>
             @foreach($clients as $client)
@@ -31,7 +31,7 @@
     </div>
 
     <div class="form-group col-xs-11{{ $errors->has('details') ? ' has-error' : '' }} mb-0 mt-3">
-        <label for="details">Project/Task details</label>
+        <label for="details">Details</label>
         <textarea rows="3" id="details" type="textarea" class="form-control" name="details" required placeholder="Project/Task details here....">{{ old('details') }}</textarea>
         @if ($errors->has('details'))
             <span class="help-block">
@@ -44,7 +44,7 @@
         <label for="status">Project Status</label>
         <select class="form-control" id="status" name="status">
             <option value="">-- Select Project Status --</option>
-            <option value="Initiated" @if (old('status') === "Initiated") {{ 'selected' }} @endif>Initiated</option>
+            <option value="Initiated" @if (old('status', "Initiated") === "Initiated") {{ 'selected' }} @endif>Initiated</option>
             <option value="Completed" @if (old('status') === "Completed") {{ 'selected' }} @endif>Completed</option>
             <option value="Pending" @if (old('status') === "Pending") {{ 'selected' }} @endif>Pending</option>
             <option value="Terminated" @if (old('status') === "Terminated") {{ 'selected' }} @endif>Terminated</option>
