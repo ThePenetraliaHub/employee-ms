9d83baaa56577dc76d561ade5b90f36ae9ca4259
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-06-07 07:45:05 +0100

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,6 +8,11 @@
     <div class="login-content col-md-10 col-md-offset-1">
         <form novalidate="novalidate" method="POST" action="{{ route('password.email') }}">
             @csrf
+            @if (session('status'))
+                <div class="alert alert-success" id="success-alert" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
             <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
                 <input value="{{ old('email') }}" type="email" id="email" autofocus class="input-field mb-0" name="email" placeholder="Email" required="" autocomplete="off">
@@ -18,38 +23,7 @@
                 @endif
             </div>
 
-            <button type="submit" class="btn btn-primary">Send Password Reset Link<i class="fa fa-lock"></i></button>
+            <button type="submit" class="btn btn-primary">Send Password Reset Link<i class="fa fa-"></i></button>
         </form>
     </div>
-@endsection
-
-@section('content')
-                <div class="card">
-                    <div class="card-header text-center text-primary">{{ __('Reset Password') }}</div>
-
-                    <div class="card-body">
-                        
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                    @if ($errors->has('email'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Send Password Reset Link') }}
-                                    </button>
-                                </div>
-                            </div>
-                </div>
 @endsection
