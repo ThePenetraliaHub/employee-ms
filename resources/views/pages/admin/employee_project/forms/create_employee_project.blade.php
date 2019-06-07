3b519564184b1c4389fa7a1d09fd379030a68193
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-06-07 01:34:24 +0100

    Update create_employee_project.blade.php

--- resources/views/pages/admin/employee_project/forms/create_employee_project.blade.php
+++ resources/views/pages/admin/employee_project/forms/create_employee_project.blade.php
@@ -1,38 +1,38 @@
 <div class="form-row">
 
     <div class="form-group col-xs-11{{ $errors->has('employee_id') ? ' has-error' : '' }} mb-0 mt-3">
-        <label for="client_id">Employee</label>
-        <select class="form-control" id="client_id" name="client_id">
-            <option value="">-- Select Client --</option>
-            @foreach($clients as $client)
-            <option value="{{$client->id}}" @if (old('client_id') == $client->id) {{ 'selected' }} @endif>{{$client->name}}</option>
+        <label for="employee_id">Employee</label>
+        <select class="form-control" id="employee_id" name="employee_id">
+            <option value="">-- Select Employee --</option>
+            @foreach($employees as $employee)
+                <option value="{{$employee->id}}" @if (old('employee_id') == $employee->id) {{ 'selected' }} @endif>{{$employee->firstname}} {{$employee->middlename}} {{$employee->lastname}}</option>
             @endforeach
         </select>
-        @if ($errors->has('client_id'))
+        @if ($errors->has('employee_id'))
             <span class="help-block">
-                <strong>{{ $errors->first('client_id') }}</strong>
+                <strong>{{ $errors->first('employee_id') }}</strong>
             </span>
         @endif
     </div>
 
     <div class="form-group col-xs-11{{ $errors->has('project_id') ? ' has-error' : '' }} mb-0 mt-3">
-        <label for="client_id">Project</label>
-        <select class="form-control" id="client_id" name="client_id">
-            <option value="">-- Select Client --</option>
-            @foreach($clients as $client)
-            <option value="{{$client->id}}" @if (old('client_id') == $client->id) {{ 'selected' }} @endif>{{$client->name}}</option>
+        <label for="project_id">Project</label>
+        <select class="form-control" id="project_id" name="project_id">
+            <option value="">-- Select Project --</option>
+            @foreach($projects as $project)
+            <option value="{{$project->id}}" @if (old('project_id') == $project->id) {{ 'selected' }} @endif>{{$project->name}}</option>
             @endforeach
         </select>
-        @if ($errors->has('client_id'))
+        @if ($errors->has('project_id'))
             <span class="help-block">
-                <strong>{{ $errors->first('client_id') }}</strong>
+                <strong>{{ $errors->first('project_id') }}</strong>
             </span>
         @endif
     </div>
 
     <div class="form-group col-xs-11{{ $errors->has('details') ? ' has-error' : '' }} mb-0 mt-3">
         <label for="details">Details</label>
-        <textarea rows="3" id="details" type="textarea" class="form-control" name="details" required placeholder="Project/Task details here....">{{ old('details') }}</textarea>
+        <textarea rows="3" id="details" type="textarea" class="form-control" name="details" required placeholder="Detail of employee task on this project here...">{{ old('details') }}</textarea>
         @if ($errors->has('details'))
             <span class="help-block">
                 <strong>{{ $errors->first('details') }}</strong>
@@ -56,7 +56,7 @@
         @endif
     </div>
 
-    <div class="form-group">
+    <div class="form-group col-xs-11 mb-0 mt-3">
         <label for="document">Project Document</label><br>
         <label class="btn btn-default" id="upload-image">
             <i class="fa fa-image{{ $errors->has('document') ? 'has-error' : '' }}"></i><span class="ml-3">Select Document</span><input name="document" id='select' type="file" style="display: none;" name="image">
