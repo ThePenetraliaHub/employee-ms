6b0a30c24c98de4b522c9f761508f18fa47382fc
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-07-08 14:56:46 +0100

    Update AdminUser.php

--- app/Mail/AdminUser.php
+++ app/Mail/AdminUser.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class AdminUser extends Mailable
+class AdminUser extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
