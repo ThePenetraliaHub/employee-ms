09441ff31eeaf389b34864f0b10b9d9ea60a8163
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-07-08 14:56:49 +0100

    Update EmployeeUser.php

--- app/Mail/EmployeeUser.php
+++ app/Mail/EmployeeUser.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class EmployeeUser extends Mailable
+class EmployeeUser extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
