3a4d4488e238b7d88d5255294ab5e76a0c552768
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-07-10 07:53:48 +0100

    Update compose.blade.php

--- resources/views/pages/all_users/messages/compose.blade.php
+++ resources/views/pages/all_users/messages/compose.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <section class="content-header panel">
-        <h1>Email
-            <small>notifications</small>
+        <h1>Message
+            <small>compose</small>
         </h1>
     </section>
 
@@ -16,25 +16,28 @@
                         <h3 class="box-title">Compose New Message</h3>
                     </div>
 
-                    <div class="box-body">
-                        <div class="form-group">
-                            <input class="form-control" placeholder="To:">
+                    <form autocomplete="off" novalidate="novalidate" role="form" id="submit_form"  enctype="multipart/form-data" method="POST" action="{{ route('certification.store') }}">
+                        @csrf
+                        <div class="box-body">
+                            <div class="form-group">
+                                <input class="form-control" placeholder="To:">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Subject:">
+                            </div>
+                            <div class="form-group">
+                                <textarea id="compose-textarea" class="form-control" style="height: 300px"></textarea>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Subject:">
-                        </div>
-                        <div class="form-group">
-                            <textarea id="compose-textarea" class="form-control" style="height: 300px"></textarea>
-                        </div>
-                    </div>
 
-                    <div class="box-footer">
-                        <div class="pull-right">
-                            <button type="button" class="btn btn-default"><i class="fa fa-pencil"></i> Draft</button>
-                            <button type="submit" class="btn btn-success"><i class="fa fa-envelope-o"></i> Send</button>
+                        <div class="box-footer">
+                            <div class="pull-right">
+                                <button name="draft" type="submit" class="btn btn-default"><i class="fa fa-pencil"></i> Draft</button>
+                                <button name="send" type="submit" class="btn btn-success"><i class="fa fa-envelope-o"></i> Send</button>
+                            </div>
+                            <a href="{{ "message.inbox" }}" class="btn btn-default"><i class="fa fa-times"></i> Discard</a>
                         </div>
-                        <button type="reset" class="btn btn-default"><i class="fa fa-times"></i> Discard</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
