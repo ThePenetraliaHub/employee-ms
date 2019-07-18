3b4be9985c3fb6d436f5af443652aed2cc001772
Nuhu Ibrahim <contactnuhuibrahim@gmail.com>
2019-07-18 23:04:57 +0100

    update for attendance

---
+++ database/migrations/2019_07_18_215255_create_work_days_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_days', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date("date");
+            $table->time("start_time");
+            $table->time("end_time");   
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_days');
+    }
+}
---
+++ database/migrations/2019_07_18_215308_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger("work_day_id");
+            $table->time("time_in");
+            $table->time("time_out");
+            $table->integer("present");
+            $table->string("absence_reason");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
