8ef24a90d620b82f800f29c709aeea1e84e2d5d7
naijamidoh <naijamidoh@gmail.com>
2019-09-16 14:46:02 +0100

    i have changed all the link icons

--- resources/views/layouts/gen-layout.blade.php
+++ resources/views/layouts/gen-layout.blade.php
@@ -80,22 +80,22 @@
                                                                           'edit_pay_grades','delete_pay_grades','browse_employee_statuses',
                                                                           'add_employee_statuses','edit_employee_statuses',
                                                                           'delete_employee_statuses']))
-                                    <li><a>Job Setup<span class="fa fa-chevron-down"></span></a>
+                                    <li><a><i class="fa fa-reorder"></i>Job Setup<span class="fa fa-chevron-down"></span></a>
                                         <ul class="nav child_menu">
                                           @if(auth()->user()->hasAnyPermission(['browse_job_titles','add_job_titles','edit_job_titles',
                                                                           'delete_job_titles']))
-                                          <li class="sub_menu"><a href="{{ route('job-title.index') }}"><i class="fa fa-users"></i>Job Titles</a></li>
+                                          <li class="sub_menu"><a href="{{ route('job-title.index') }}"><i class="fa fa-keyboard-o"></i>Job Titles</a></li>
                                           @endif
 
                                           @if(auth()->user()->hasAnyPermission(['browse_pay_grades','add_pay_grades',
                                                                           'edit_pay_grades','delete_pay_grades']))
-                                          <li><a href="{{ route('pay-grade.index') }}">Pay Grades</a></li>
+                                          <li><a href="{{ route('pay-grade.index') }}"><i class="fa fa-pencil-square"></i>Pay Grades</a></li>
                                           @endif
 
                                           @if(auth()->user()->hasAnyPermission(['browse_employee_statuses',
                                                                           'add_employee_statuses','edit_employee_statuses',
                                                                           'delete_employee_statuses']))
-                                          <li><a href="{{ route('employee-status.index') }}">Employment Statuses</a></li>
+                                          <li><a href="{{ route('employee-status.index') }}"><i class="fa fa-puzzle-piece"></i>Empt. Statuses</a></li>
                                           @endif
 
                                         </ul>
@@ -110,17 +110,17 @@
 
                                   @if(auth()->user()->hasAnyPermission(['browse_employee_skills','add_employee_skills','edit_employee_skills',
                                                                               'delete_employee_skills']))
-                                    <li><a href="{{ route('skills.index') }}"><i class=""></i> Manage Skills</a></li>
+                                    <li><a href="{{ route('skills.index') }}"><i class=""></i><i class="fa fa-gears"></i> Manage Skills</a></li>
                                   @endif
 
                                   @if(auth()->user()->hasAnyPermission(['browse_employee_certifications','add_employee_certifications','edit_employee_certifications',
                                                                               'delete_employee_certifications','download_employee_certifications']))                           
-                                    <li><a href="{{ route('certification.index') }}"><i class="glyphicon glyphicon-certificate"></i> Manage Certifications</a></li>                          
+                                    <li><a href="{{ route('certification.index') }}"><i class="fa fa-certificate"></i> Manage Certifications</a></li>                          
                                   @endif
 
                                   @if(auth()->user()->hasAnyPermission(['browse_employee_educations','add_employee_educations','edit_employee_educations',
                                                                               'delete_employee_educations','download_employee_educations']))
-                                    <li><a href="{{ route('education.index') }}"><i class="glyphicon glyphicon-education"></i> Manage Educations</a></li>
+                                    <li><a href="{{ route('education.index') }}"><i class="fa fa-graduation-cap"></i> Manage Educations</a></li>
                                   @endif
 
                                   @if(auth()->user()->hasAnyPermission(['browse_projects','add_projects',
@@ -128,12 +128,12 @@
                                                                           'add_employee_tasks','edit_employee_tasks',
                                                                           'delete_employee_tasks','download_employee_tasks']))
 
-                                    <li><a><i class="fa fa-tasks"></i>Project Setup<span class="fa fa-chevron-down"></span></a>
+                                    <li><a><i class="fa fa-file-text-o"></i>Project Setup<span class="fa fa-chevron-down"></span></a>
                                         <ul class="nav child_menu">
                                           @if(auth()->user()->hasAnyPermission(['browse_projects','add_projects',
                                                                   'edit_projects','delete_projects']))
                                           <li>
-                                              <a href="{{ route('projects.index') }}"><i class=""></i> Projects
+                                              <a href="{{ route('projects.index') }}"><i class="fa fa-book"></i> Projects
                                               </a>
                                           </li>
                                           @endif
@@ -141,7 +141,7 @@
                                                                   'add_employee_tasks','edit_employee_tasks',
                                                                   'delete_employee_tasks','download_employee_tasks']))
                                           <li>
-                                              <a href="{{ route('employee-project.index') }}"><i class=""></i> Employee Tasks
+                                              <a href="{{ route('employee-project.index') }}"><i class="fa fa-external-link"></i> Employee Tasks
                                               </a>
                                           </li>
                                           @endif
@@ -175,30 +175,30 @@
                                                                       'delete_employee_roles','download_employee_roles',
                                                                       'browse_administrator_roles','add_administrator_roles','edit_administrator_roles',
                                                                       'delete_administrator_roles','download_administrator_roles']))
-                            <li><a><i class="fa fa-edit"></i> System <span class="fa fa-chevron-down"></span></a>
+                            <li><a><i class="fa fa-laptop"></i> System <span class="fa fa-chevron-down"></span></a>
                               <ul class="nav child_menu">
-                                  <li><a>Employee<span class="fa fa-chevron-down"></span></a>
+                                  <li><a><i class="fa fa-user"></i>Employee<span class="fa fa-chevron-down"></span></a>
                                       <ul class="nav child_menu">
                                       @if(auth()->user()->hasAnyPermission(['browse_employee_user','add_employee_user','read_employee_user',
                                                                       'edit_employee_user','delete_employee_user',
                                                                       'activate_deactivate_employee_user']))
-                                              <li><a href="{{ route('user.index') }}"><i class=""></i> Employee Users</a></li>
+                                              <li><a href="{{ route('user.index') }}"><i class="fa fa-users"></i> Employee Users</a></li>
                                       @endif
                                       @if(auth()->user()->hasAnyPermission(['browse_employee_roles','add_employee_roles','edit_employee_roles',
                                                                   'delete_employee_roles','download_employee_roles']))
-                                              <li><a href="{{ route('role.employee') }}"><i class=""></i> Employee Roles</a></li>
+                                              <li><a href="{{ route('role.employee') }}"><i class="fa fa-magic"></i> Employee Roles</a></li>
                                       @endif 
                                       </ul>
                                   </li>
-                                  <li><a>Administrator<span class="fa fa-chevron-down"></span></a>
+                                  <li><a><i class="fa fa-male"></i>Administrator<span class="fa fa-chevron-down"></span></a>
                                       <ul class="nav child_menu">
                                       @if(auth()->user()->hasAnyPermission(['browse_admin_user','add_admin_user','read_admin_user',
                                                                   'edit_admin_user','delete_admin_user','activate_deactivate_admin_user']))
-                                              <li><a href="{{ route("admin.index") }}"><i class="fa fa-circle-o"></i> Admin Users</a></li>
+                                              <li><a href="{{ route("admin.index") }}"><i class="fa fa-users"></i> Admin Users</a></li>
                                       @endif
                                       @if(auth()->user()->hasAnyPermission(['browse_administrator_roles','add_administrator_roles','edit_administrator_roles',
                                                                   'delete_administrator_roles','download_administrator_roles']))
-                                              <li><a href="{{ route('role.admin') }}"><i class="fa fa-circle-o"></i> Admin Roles</a></li>
+                                              <li><a href="{{ route('role.admin') }}"><i class="fa fa-magic"></i> Admin Roles</a></li>
                                       @endif
                                       </ul>
                                   </li>
@@ -212,24 +212,24 @@
                                                                         'add_working_days', 'edit_working_days',
                                                                         'delete_working_days','download_working_days']))
                                   <li>
-                                    <a href="{{ route('work-day.index') }}"><i class="fa fa-user"></i> Working Days</a>
+                                    <a href="{{ route('work-day.index') }}"><i class="fa fa-calendar"></i> Working Days</a>
                                   </li>
                                   @endif
 
                                   @if(auth()->user()->hasAnyPermission(['sign_in_employee','sign_out_employee']) )
                                   <li>
-                                    <a href="{{ route('attendance.index') }}"><i class="fa fa-user"></i> Sign In | Sign Out</a>
+                                    <a href="{{ route('attendance.index') }}"><i class="fa fa-toggle-on"></i> Sign In | Sign Out</a>
                                   </li>
                                   @endif
 
                                   @if(auth()->user()->can('browse_attendance_general_report') )
                                   <li>
-                                    <a href="{{ route('attendance.general_report') }}"><i class="fa fa-user"></i> General Report</a>
+                                    <a href="{{ route('attendance.general_report') }}"><i class="fa fa-file-text"></i> General Report</a>
                                   </li>
                                   @endif
 
                                   <li>
-                                    <a href="{{ route('attendance.self_attendance') }}"><i class="fa fa-user"></i> My Attendance</a>
+                                    <a href="{{ route('attendance.self_attendance') }}"><i class="fa fa-folder-o"></i> My Attendance</a>
                                   </li>
                               </ul>
                             </li>
@@ -240,7 +240,7 @@
                                 @if(auth()->user()->hasAnyPermission(['browse_leave_profiles','read_leave',
                                                                 'add_leave','edit_leave','delete_leave']))
                                 <li>
-                                  <a href="{{ route('leave-type.index') }}"><i class="glyphicon glyphicon-list-alt"></i> Leave Profile</a>
+                                  <a href="{{ route('leave-type.index') }}"><i class="fa fa-file-text-o"></i> Leave Profile</a>
                                 </li>
                                 @endif
 
@@ -249,14 +249,14 @@
                                                                   'browse_leave_profiles','approve_unapprove_leave']))
 
                                 <li>
-                                  <a href="{{ route('leave-request.index') }}"><i class="glyphicon glyphicon-send"></i> Leave Request</a>
+                                  <a href="{{ route('leave-request.index') }}"><i class="fa fa-mail-forward"></i> Leave Request</a>
                                 </li>
                                 @endif
 
                                 @if(auth()->user()->can('approve_unapprove_leave') )
 
                                 <li>
-                                  <a href="{{ route('leave-approval.index') }}"><i class="glyphicon glyphicon-send"></i> Leave Approval</a>
+                                  <a href="{{ route('leave-approval.index') }}"><i class="fa fa-mail-reply"></i> Leave Approval</a>
                                 </li>
                                 @endif
                               </ul>
@@ -273,7 +273,7 @@
                               </form>
                             <li>
                               <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                              <i class="glyphicon glyphicon-log-out"></i><span> Logout</span>
+                              <i class="fa fa-sign-out"></i><span> Logout</span>
                               </a>
                             </li>
                         </ul>
